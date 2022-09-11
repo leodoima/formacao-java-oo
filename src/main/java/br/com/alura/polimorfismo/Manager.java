21eb24e1ec790c42ab43b4bbc4d8472cc4eddac3
Leonardo dos Santos <leodoima@hotmail.com>
2022-09-11 20:32:45 -0300

    Entendendo polimorfismo

--- src/main/java/br/com/alura/polimorfismo/Manager.java
+++ src/main/java/br/com/alura/polimorfismo/Manager.java
@@ -2,6 +2,11 @@ package br.com.alura.polimorfismo;
 
 public class Manager extends Employee {
 
+
+    private String login;
+    private String password;
+
+
     public Manager(String name, String cpf, double salary) {
         super(name, cpf, salary);
     }
@@ -12,5 +17,21 @@ public class Manager extends Employee {
 
     public double getBonification() {
         return super.getBonification() + super.getSalary() * 0.3;
+    }
+
+    public boolean authenticationAccess(String login, String password) {
+
+        boolean loginAccess = (login.equalsIgnoreCase(this.login));
+        boolean passwordAccess = (password.equals(this.password));
+
+        return (loginAccess && passwordAccess);
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
