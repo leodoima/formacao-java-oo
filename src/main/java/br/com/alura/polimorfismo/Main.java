71ede7f9d74342ce01242b5329873b12e916dff3
Leonardo dos Santos <leodoima@hotmail.com>
2022-09-11 18:08:16 -0300

    Criada nova branch para polimorfismo

--- src/main/java/br/com/alura/polimorfismo/Main.java
+++ src/main/java/br/com/alura/polimorfismo/Main.java
@@ -3,7 +3,7 @@ package br.com.alura.polimorfismo;
 public class Main {
     public static void main(String[] args) {
 
-        Employee employeeNico = new Employee("Nico Risberg", "000.999.888-11", 2000);
+        Employee employeeNico = new Employee("Nico Rosberg", "000.999.888-11", 2000);
         System.out.println(employeeNico.getBonification());
 
         Manager managerMauro = new Manager("Mauro da Silveira", "111.222.111-33");
@@ -11,6 +11,5 @@ public class Main {
         managerMauro.setSalary(10000.0);
         System.out.println(managerMauro.getSalary());
         System.out.println(managerMauro.getBonification());
-
     }
 }
